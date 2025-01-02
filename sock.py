18867725ff614bcd7493a07d77ecf43e38492dbd
Bobyuw <3593045939@qq.com>
2025-01-02 11:51:09 +0800

    add socket

--- sock.py
+++ sock.py
@@ -1,13 +1,25 @@
 import socket
+import threading
+import time
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12312
-s.bind((host, port))
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 9999))
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
 s.listen(5)
-c,addr = s.accept()
-seat_number = c.recv(1024)
-c.send('OK')
+print('Waiting for connection...')
 while True:
-    c.recv(1024)
+    # 接受一个新连接:
+    sock, addr = s.accept()
+    # 创建新线程来处理TCP连接:
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
