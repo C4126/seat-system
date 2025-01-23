72cd7a7ca1b1196d66f9e1544afbc103755bd101
Fu Yuchen <fuyuchen0904@163.com>
2025-01-23 21:04:12 +0800

    update(hardware): fit new API

--- hardware.ino
+++ hardware.ino
@@ -1,4 +1,3 @@
-//客户端
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -6,47 +5,37 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-constexpr uint8_t RST_PIN = D3;  // Configurable, see typical pin layout above
-constexpr uint8_t SS_PIN = D8;   // Configurable, see typical pin layout above
+constexpr uint8_t RST_PIN = D3;
+constexpr uint8_t SS_PIN = D8;
 
 MFRC522_SPI spiDevice = MFRC522_SPI(SS_PIN, RST_PIN);
-MFRC522 mfrc522 = MFRC522(&spiDevice);  // Create MFRC522 instance
+MFRC522 mfrc522 = MFRC522(&spiDevice);
 
 #define WIFISSID "Redmi_FA9A"
 #define PASSWD "PASSWORD"
 
-#define ID 1
-
-const char* host = "";  //服务端的ip地址
+const char* host = "";
 
 Ticker flipper;
 WiFiClient client;
 #include "SSD1306Spi.h"
 #define ID_LEN (sizeof(mfrc522.uid.uidByte) * 400)
 
-
-/* 新建一个oled屏幕对象，需要输入IIC地址，SDA和SCL引脚号 */
 SSD1306Spi oled(D0, D4, D1);
 
 char status[100][100];
 
-//发送心跳包（随意发送一次数据） 用于保持连接
 void flip() {
   int n = 0;
   int cnt = 0;
   client.print("<get-status>");
   while (client.available() > 0) {
     char c = client.read();
-    if (c == '<') n = 1;
+    if (c == '<') n = 2;
     else if (c == ';' && n != -1 && n != 0) {
       status[n - 2][cnt] = 0;
       n++;
       cnt = 0;
-    } else if (n == 1) {
-      if (c - '0' != ID) {
-        n = -1;
-        continue;
-      }
     } else if (n > 1) {
       status[n - 2][cnt] = c;
       cnt++;
@@ -78,31 +67,30 @@ void setup() {
 
   Serial.println("Initalizing OLED...\n");
 
-  /* 2. oled屏幕初始化 */
   oled.init();
-  oled.flipScreenVertically();  // 设置屏幕翻转
-  oled.setContrast(255);        // 设置屏幕亮度
+  oled.flipScreenVertically();
+  oled.setContrast(255);
   Serial.println("Connecting...\n");
   WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFISSID, PASSWD);       // change it to your ussid and password
-  mfrc522.PCD_Init();                 // Init MFRC522
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  WiFi.begin(WIFISSID, PASSWD);
+  mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-  drawRect();  // 测试屏幕显示
+  drawRect();
   oled.clear();
-  oled.display();  // 清除屏幕
+  oled.display();
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  if (!client.connect("192.168.31.233", 1234))  //连接的IP地址和
+  if (!client.connect("192.168.31.233", 1234))
   {
     Serial.println("Connection to host failed");
     delay(1000);
     return;
   }
-  flipper.attach(10, flip);  //每隔180秒执行一次回调函数
+  flipper.attach(10, flip);
 }
 
 
@@ -135,7 +123,6 @@ char* check_for_card() {
 
 
   // disengage with the card.
-  //
   mfrc522.PICC_HaltA();
   return tag;
 }
@@ -144,8 +131,6 @@ void loop() {
   char* s = check_for_card();
   if (s != NULL) {
     client.print("<");
-    client.print(ID);
-    client.print(';');
     client.print(s);
     client.print(">");
   }
