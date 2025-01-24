0b4b50759ac9151a0e0a0c0cf65d822ff9db04a0
Bobyuw <3593045939@qq.com>
2025-01-24 22:06:39 +0800

    修改main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import socket
 import threading
 seat=[]
-
 def create_server_socket(host, port):
     socket_server = socket.socket()
     socket_server.bind((host, port))
@@ -9,7 +8,8 @@ def create_server_socket(host, port):
     print(f"服务端已启动，地址{host}，端口{port}")
     print(f"正在等待客户端连接...")
     # 开启多线程，收发来自多个客户端的数据
-    num = 0  # 标记客户端的编号
+    global num
+    num = 0
     while True:
         conn, address = socket_server.accept()
         print(f"服务端已接受到客户端 {num}号 的连接请求，客户端信息：{address}")
@@ -50,67 +50,32 @@ def handle_client(conn, address, num):
             data = data.replace("<", "")
             data = data.replace(">", "")
             if data in seat[num]:
+                print("remove "+data)
                 seat[num].remove(data)
-                conn.send("OK".encode("UTF-8"))
+                #conn.send("OK".encode("UTF-8"))
                 member_num -= 1
                 continue
             else:
                 if member_num >= 4:
-                    conn.send("fail_out".encode("UTF-8"))
+                    print("fail_out")
+                    #conn.send("fail_out".encode("UTF-8"))
                     continue
+                print("add "+data)
                 member_num += 1
                 seat[num].append(data)
-                conn.send("OK".encode("UTF-8"))
+                #conn.send("OK".encode("UTF-8"))
                 continue
-''' 
-   while True:
-        # 接收客户端发来的数据
-        data_from_client: str = conn.recv(1024).decode("UTF-8")
-        if data_from_client == 'exit':
-            break
-        if data_from_client == 'get_status':
-            print('get_status')
-            tmp_string=""
-            tmp_num=0
-            for i in seat[num]:
-                tmp_num+=1
-                tmp_string=tmp_string+i
-                if tmp_num!=4:
-                    tmp_string=tmp_string+";"
-            while(tmp_num<4):
-                tmp_num+=1
-                tmp_string=tmp_string+"?"
-                if tmp_num!=4:
-                    tmp_string=tmp_string+";"
-            conn.send(tmp_string.encode("UTF-8"))
-            continue
-        if data_from_client == 'add_user':
-            print('add_user')
-            conn.send("add_user".encode("UTF-8"))
-            member_num+=1
-            if member_num>=5:
-                conn.send('fail_out'.encode("UTF-8"))
-                continue
-            else:
-                member: str = conn.recv(1024).decode("UTF-8")
-                print(member)
-                seat[num].append(member)
-                conn.send("OK".encode("UTF-8"))
-                continue
-        if data_from_client == "remove_user":
-            print("remove_user")
-            conn.send("remove_user".encode("UTF-8"))
-            member:str = conn.recv(1024).decode("UTF-8")
-            if member in seat[num]:
-                seat[num].remove(member)
-                conn.send("OK".encode("UTF-8"))
-                member_num-=1
-                continue
-            else:
-                conn.send("fail_no_user".encode("UTF-8"))
-                continue
-    conn.close()
-    '''
+
+def add_member(data):
+    for i in range(num):
+        if seat[num].len() == 0:
+            seat[num].append(data)
+            return
+    else:
+        for i in range(num):
+            if seat[num].len() != 4:
+                seat[num].append(data)
+                return
 
 if __name__ == '__main__':
     server_host = "127.0.0.1"
